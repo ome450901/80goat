504ec7ab09e0864af80e33973d699a9d1f370320
WillyYu <ome450901@gmail.com>
2017-02-25 11:44:02 +0800

    Close auto reply same message

--- index.js
+++ index.js
@@ -18,18 +18,6 @@ var bot = linebot({
     channelAccessToken: CHANNEL_ACCESS_TOKEN
 });
 
-bot.on('message', function (event) {
-    if (event.message.type = 'text') {
-        event.reply(event.message.text).then(function (data) {
-            // success
-        }).catch(function (error) {
-            // error
-        });
-    }
-    console.log(event);
-    console.log(event.message.content);
-});
-
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
 
@@ -38,5 +26,28 @@ var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
 });
+
+bot.on('message', function (event) {
+    // if (event.message.type = 'text') {
+    //     event.reply(event.message.text).then(function (data) {
+    //         // success
+    //     }).catch(function (error) {
+    //         // error
+    //     });
+    // }
+    console.log(event);
+    console.log(event.message.content);
+});
+
+function processMessage(message) {
+    var replyMessage = '';
+    if (message.indexOf("閉嘴")) {
+        event.reply(replyMessage).then(function (data) {
+            // success
+        }).catch(function (error) {
+            // error
+        });
+    }
+}
 
 module.exports = app;
